0cdd84d02b3ad2ac6c21f3cbca3c410f0c22aee6
Andy Sturges <arsturges@lbl.gov>
2011-07-17 20:15:12 -0700

    added new page

--- et0.rb
+++ et0.rb
@@ -19,5 +19,14 @@ get '/calculation' do
 end
 
 get '/what-is-evapotranspiration' do
+  @monthly_base = CSV.read("public/monthly_base.csv")
+  (0..@monthly_base.length-1).each do |row|
+    @january_in_California = @monthly_base[row][7].to_f if @monthly_base[row][0] == "CA" && @monthly_base[row][3].to_i == 1
+    @january_in_Ohio = @monthly_base[row][7].to_f if @monthly_base[row][0] == "OH" && @monthly_base[row][3].to_i == 1
+  end
   erb :what_is_evapotranspiration
+end
+
+get '/how-is-evapotranspiration-used' do
+  erb: :how_is_evapotranspiration_used
 end
